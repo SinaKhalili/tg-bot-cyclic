1806455f7f14f3202e8fde9d13526632c4f97c38
sina <khalili@sfu.ca>
2023-11-22 02:57:05 -0800

    Update bot

--- src/bot.ts
+++ src/bot.ts
@@ -167,8 +167,8 @@ bot.api.setMyCommands([
 ]);
 
 // Handle all other messages and the /start command
-const introductionMessage = `Hello! I'm a Telegram bot.
-I'm powered by Cyclic, the next-generation serverless computing platform.
+const introductionMessage = `
+beep booop beeeeeeep booooooooooooooop
 
 <b>Commands</b>
 /yo - Be greeted by me
